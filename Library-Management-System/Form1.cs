db26bbbc2bb31f5adea36e5b3ea20563d9fafc25
K.A.D.N. Kodikara <kadnkodikara@students.nsbm.ac.lk>
2021-06-29 01:55:01 +0530

    Add Home, About Screen Frontend

--- Library-Management-System/Form1.cs
+++ Library-Management-System/Form1.cs
@@ -16,5 +16,10 @@ namespace Library_Management_System
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
